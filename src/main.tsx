4e311560596129695ed90ff4e01ec76d7656457d
emammad22 <mammadoveltun22@gmail.com>
2024-02-14 02:09:28 +0400

    null push

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root-d")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root-d")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
